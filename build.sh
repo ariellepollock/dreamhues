d9c836264ece3a0d14255b59f7d12ec6f91fcff9
ariellepollock <ariellecpollock@gmail.com>
2024-02-01 15:49:39 -0600

    pip3 freeze requirements

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+python3 manage.py collectstatic --no-input
+python3 manage.py migrate
